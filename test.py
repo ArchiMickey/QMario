849da0dbc202cc906fdcb06f4edb8623a268a4af
ArchiMickey <wuchunyat119@gmail.com>
2022-06-11 03:55:20 +0800

    updated test script

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from core.model import DDQNLightning
 
 model = DDQNLightning(
     batch_size=256,
-    warm_start_size=500,
-    episode_length=500,
+    warm_start_size=300,
+    episode_length=300,
     replay_size=10000,
     eps_min=0.01,
 )
 
 trainer = pl.Trainer(
-    accelerator="gpu"
+    accelerator="gpu",
 )
-trainer.test(model=model, ckpt_path="model/qmario-epoch=17291.ckpt")
+trainer.test(model=model, ckpt_path="model/qmario-epoch=34999.ckpt")
